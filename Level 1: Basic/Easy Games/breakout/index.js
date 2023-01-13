1c8136b2bbcb382ee6aad07535d5860828767270
itsmaaroufwork <itsmaarouf@gmail.com>
2023-01-13 10:54:28 +0100

    ball & user

--- Level 1: Basic/Easy Games/breakout/index.js
+++ Level 1: Basic/Easy Games/breakout/index.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector('.grid');
 const blockWidth = 100;
 const blockHeight = 20;
-
+const boardWidth = 560;
 const userStart = [230, 10];
 let currentPosition = userStart;
+let timeId;
+
+const ballStart = [270, 40];
+let currentBallPosition = ballStart;
 
 // class Block
 class Block {
@@ -34,9 +38,6 @@ const Blocks = [
     new Block(450, 210),
 ]
 
-console.log(2 + 2 + 3);
-console.log(Blocks[0])
-
 // Create a block
 function addBlock() {
     for (let i = 0; i < Blocks.length; i++) {
@@ -55,6 +56,53 @@ addBlock()
 
 const user = document.createElement('div');
 user.classList.add('user');
-user.style.left = currentPosition[0] + 'px';
-user.style.bottom = currentPosition[1] + 'px';
+drawUser();
 grid.appendChild(user);
+
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+function drawBall() {
+    ball.style.left = currentBallPosition[0] + 'px';
+    ball.style.bottom = currentBallPosition[1] + 'px';
+}
+
+function MoveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawUser();
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] + blockWidth < boardWidth) {
+                currentPosition[0] += 10
+                drawUser();
+            }
+            break;
+        case 'ArrowDown':
+            if (currentPosition[1] > 10) {
+                currentPosition[1] -= 10
+                drawUser();
+            }
+            break;
+        case 'ArrowUp':
+            if (currentPosition[1] < 150) {
+                currentPosition[1] += 10
+                drawUser();
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', MoveUser);
+
+// create a ball
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall()
+grid.appendChild(ball);
+
