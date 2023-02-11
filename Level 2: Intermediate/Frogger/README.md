2e23ada33309a0541bd9be9874cbb8b1ffdf19a8
itsmaarouf <itsmaarouf@gmail.com>
2023-02-11 10:58:39 +0100

    end Frogger game

---
+++ Level 2: Intermediate/Frogger/README.md
@@ -0,0 +1,9 @@
+# Frogger
+
+"Welcome to **Frogger!** The objective of this game is to help a frog cross a busy road and reach its home. The frog must avoid cars and trucks moving in different directions on the road, as well as Dragons and Hippo :D in the river.
+
+To play the game, use the arrow keys on your keyboard to control the frog's movements. Move the frog up, down, left, or right to navigate around obstacles. Make sure to reach the other side of the road without getting hit by any of the moving vehicles or falling into the water.
+
+Every time the frog reaches its home, it earns 1 point and 5 additional seconds. The game ends if the frog is hit by a car, falls into the water, or if time runs out.
+
+> Good luck, and have fun playing **Frogger!**"
--- Level 2: Intermediate/Frogger/index.js
+++ Level 2: Intermediate/Frogger/index.js
@@ -1,13 +1,14 @@
 const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseBtn = document.querySelector('#start-pause');
+const gameOver = document.querySelector('.gameOver');
 const gridWidth = 9;
 let score = 0;
 const grid = document.querySelector('.grid');
 let currentIndex = 76;
 let timerId;
 // game time
-let gameTime = 10;
+let gameTime = 60;
 let currentTime = gameTime;
 // to control the winning and losing
 let controlTimerId;
@@ -20,35 +21,37 @@ function CreateCars(x) {
     return x % (Math.round(gridWidth / 3)) + 1
 }
 
-// creating all the Blocks inside the grid
+// creating all the Blo| Score: cks inside the grid
 
 for (let i = 0; i < gridWidth; i++) {
     for (let j = 0; j < gridWidth; j++) {
         const div = document.createElement('div');
         grid.appendChild(div)
-        if (i == 0 && j == 4) {
-            div.classList.add('ending-block');
-        }
+        if (i == 0 && j == 4) div.classList.add('ending-block', 'fa-solid', 'fa-tent');
+        if (i == 0 || i == 1 || i == 4) div.classList.add('grass');
         if (i == 2) {
             div.classList.add('log-left');
             div.classList.add('l' + Barriers(j));
+            if (div.classList.contains('l4') || div.classList.contains('l5')) div.classList.add('fa-solid', 'fa-hippo', 'fa-flip-horizontal');
         }
         if (i == 3) {
             div.classList.add('log-right');
-            div.classList.add('l' + Barriers(j));
+            div.classList.add('l' + Barriers(j + 2));
+            if (div.classList.contains('l4') || div.classList.contains('l5')) div.classList.add('fa-solid', 'fa-dragon');
         }
         if (i == 5) {
             div.classList.add('car-left');
             div.classList.add('c' + CreateCars(j));
+            if (div.classList.contains('c1')) div.classList.add('fa-solid', 'fa-car-side', 'fa-flip-horizontal');
+
         }
         if (i == 6) {
             div.classList.add('car-right');
-            div.classList.add('c' + CreateCars(j));
+            div.classList.add('c' + CreateCars(j + 2));
+            if (div.classList.contains('c1')) div.classList.add('fa-solid', 'fa-truck-fast');
         }
 
-        if (i == 8 && j == 4) {
-            div.classList.add('starting-block');
-        }
+        if (i == 7 || i == 8) div.classList.add('theStart');
 
     }
 }
@@ -61,7 +64,7 @@ const carsRight = document.querySelectorAll('.car-right');
 
 function moveFrog(e) {
     //removing the class frog from the grid element
-    squares[currentIndex].classList.remove('frog');
+    squares[currentIndex].classList.remove('frog', 'fa-solid', 'fa-frog');
 
     switch (e.key) {
         case 'ArrowLeft':
@@ -84,10 +87,10 @@ function moveFrog(e) {
             break;
     }
     //add the class frog from the grid element
-    squares[currentIndex].classList.add('frog');
+    squares[currentIndex].classList.add('frog', 'fa-solid', 'fa-frog');
 }
 //add the class frog from the grid element
-squares[currentIndex].classList.add('frog');
+squares[currentIndex].classList.add('frog', 'fa-solid', 'fa-frog');
 
 function autoMoveElement() {
     currentTime--;
@@ -102,45 +105,27 @@ function control() {
     lose();
     win();
 }
-// move logs to the left side
+// move hippo to the left side
 function moveLogLeft(logsLeft) {
-    /*switch (true) {
-        case logsLeft.classList.contains('l1'):
-            logsLeft.classList.remove('l1');
-            logsLeft.classList.add('l2');
-            break;
-        case logsLeft.classList.contains('l2'):
-            logsLeft.classList.remove('l2');
-            logsLeft.classList.add('l3');
-            break;
-        case logsLeft.classList.contains('l3'):
-            logsLeft.classList.remove('l3');
-            logsLeft.classList.add('l4');
-            break;
-        case logsLeft.classList.contains('l4'):
-            logsLeft.classList.remove('l4');
-            logsLeft.classList.add('l5');
-            break;
-        case logsLeft.classList.contains('l5'):
-            logsLeft.classList.remove('l5');
-            logsLeft.classList.add('l1');
-            break;
-    }*/
     for (let i = 1; i <= 5; i++) {
         if (logsLeft.classList.contains('l' + i)) {
+            if (logsLeft.classList.contains('l4') || logsLeft.classList.contains('l5')) logsLeft.classList.remove('fa-solid', 'fa-hippo');
             logsLeft.classList.remove('l' + i++);
             if (i > 5) i = 1
             logsLeft.classList.add('l' + i);
+            if (logsLeft.classList.contains('l4') || logsLeft.classList.contains('l5')) logsLeft.classList.add('fa-solid', 'fa-hippo', 'fa-flip-horizontal');
         }
     }
 }
-// move logs to the right side
+// move dragon to the right side
 function moveLogRight(logsRight) {
     for (let i = 5; i >= 1; i--) {
         if (logsRight.classList.contains('l' + i)) {
+            if (logsRight.classList.contains('l4') || logsRight.classList.contains('l5')) logsRight.classList.remove('fa-solid', 'fa-dragon');
             logsRight.classList.remove('l' + i--);
             if (i === 0) i = 5
             logsRight.classList.add('l' + i);
+            if (logsRight.classList.contains('l4') || logsRight.classList.contains('l5')) logsRight.classList.add('fa-solid', 'fa-dragon');
         }
     }
 }
@@ -148,19 +133,24 @@ function moveLogRight(logsRight) {
 function moveCarLeft(carsLeft) {
     for (let i = 1; i <= 3; i++) {
         if (carsLeft.classList.contains('c' + i)) {
+            if (carsLeft.classList.contains('c1')) carsLeft.classList.remove('fa-solid', 'fa-car-side');
             carsLeft.classList.remove('c' + i++);
             if (i > 3) i = 1
             carsLeft.classList.add('c' + i);
+            if (carsLeft.classList.contains('c1')) carsLeft.classList.add('fa-solid', 'fa-car-side', 'fa-flip-horizontal');
+
         }
     }
 }
-// move cars to the right side   
+// move truck to the right side   
 function moveCarRight(carsRight) {
     for (let i = 3; i >= 1; i--) {
         if (carsRight.classList.contains('c' + i)) {
+            if (carsRight.classList.contains('c1')) carsRight.classList.remove('fa-solid', 'fa-truck-fast');
             carsRight.classList.remove('c' + i--);
             if (i === 0) i = 3
             carsRight.classList.add('c' + i);
+            if (carsRight.classList.contains('c1')) carsRight.classList.add('fa-solid', 'fa-truck-fast');
         }
     }
 }
@@ -172,7 +162,7 @@ function lose() {
         squares[currentIndex].classList.contains('c1') ||
         currentTime === 0
     ) {
-        resultDisplay.textContent = "Game over";
+        gameOver.textContent = "Game over";
         clearInterval(timerId);
         clearInterval(controlTimerId);
         timerId, controlTimerId = null;
@@ -183,11 +173,12 @@ function lose() {
 
 function win() {
     if (squares[currentIndex].classList.contains('ending-block')) {
-        resultDisplay.textContent = ++score;
+        score++;
+        resultDisplay.textContent = ' | Score: ' + score;
         squares[currentIndex].classList.remove('frog')
-        currentTime = --gameTime;
+        currentTime = currentTime + 5;
         currentIndex = 76;
-        squares[currentIndex].classList.add('frog')
+        squares[currentIndex].classList.add('frog', 'fa-solid', 'fa-frog')
     }
 }
 startPauseBtn.addEventListener('click', () => {
@@ -206,4 +197,3 @@ startPauseBtn.addEventListener('click', () => {
         document.addEventListener('keyup', moveFrog);
     }
 })
-
