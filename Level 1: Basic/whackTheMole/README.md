eb60067a9ffdac5493178da9337b6fc5936a85f1
itsmaarouf <itsmaarouf@gmail.com>
2023-02-22 09:53:37 +0100

    end first level game

---
+++ Level 1: Basic/whackTheMole/README.md
@@ -0,0 +1,9 @@
+# Whack the mole (mosquito)
+
+> You must click on the mosquito to earn the point
+
+![game start](img/game-start.png)
+
+> in the end of the game you get the final score
+
+![player is winner](img/game-over.png)
--- Level 1: Basic/whackTheMole/index.js
+++ Level 1: Basic/whackTheMole/index.js
@@ -1,21 +1,28 @@
 const boxes = 9;
+// create a div and save it in grid variable
 const grid = document.createElement('div');
+// add class "grid" to grid
 grid.classList.add('grid');
 
+// create all the necessary boxes 
 for (let i = 0; i < boxes; i++) {
     const div = document.createElement('div');
+    //and add them to the grid
     grid.appendChild(div);
 }
 
+// printing the boxes in the game
 document.getElementById("game").appendChild(grid);
+// select all the boxes and save them in the square variable
 const squares = document.querySelectorAll(".grid div");
 
-squares.forEach( square => square.classList.add("square"))
-
-for(i = 0 ; i < squares.length; i++){
-    squares[i].setAttribute("id",i+1) 
+// add class "square" to every box in the game
+squares.forEach(square => square.classList.add("square"))
+// add id number to every box in the game
+for (i = 0; i < squares.length; i++) {
+    squares[i].setAttribute("id", i + 1)
 }
-
+// declare the variables
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score');
@@ -25,17 +32,17 @@ let hitPositions;
 let currentTime = 60;
 let timerId = null;
 
+// function to moving the active box randomly
 function randomSquare() {
-    squares.forEach( square => square.classList.remove('mole'));
-
+    squares.forEach(square => square.classList.remove('mole', 'fa-solid', 'fa-mosquito'));
     let randomSquare = squares[Math.floor(Math.random() * squares.length)]
-    randomSquare.classList.add('mole');
+    randomSquare.classList.add('mole', 'fa-solid', 'fa-mosquito', 'fa-9x');
     hitPositions = randomSquare.id;
 }
-
+// get plus point
 squares.forEach(square => {
-    square.addEventListener('mousedown', ()=> {
-        if(hitPositions==square.id){
+    square.addEventListener('mousedown', () => {
+        if (hitPositions == square.id) {
             result++;
             currentTime++;
             score.textContent = result;
@@ -43,14 +50,14 @@ squares.forEach(square => {
         }
     });
 })
-
-function moveMole(){
-    timerId = setInterval(randomSquare,500)
+// moving the active box
+function moveMole() {
+    timerId = setInterval(randomSquare, 900)
 }
-
+// call moveMole function to move the active boxes
 moveMole();
-
-function countDown(){
+// countdown: if timer = 0 then the game is over
+function countDown() {
     currentTime--;
     timeLeft.textContent = currentTime;
 
@@ -60,5 +67,5 @@ function countDown(){
         alert('Game over! Your final score is ' + result);
     }
 }
-
-let countDownTimerId = setInterval(countDown,1000)
+// calling countDown function every 1 second
+let countDownTimerId = setInterval(countDown, 1000)
