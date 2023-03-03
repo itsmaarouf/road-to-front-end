3334f3e70229b00802eb9472b8d6ab83ede8832e
itsmaarouf <itsmaarouf@gmail.com>
2023-03-03 07:44:44 +0100

    invaders

---
+++ Level 2: Intermediate/spaceInvaders/README.md
@@ -0,0 +1,15 @@
+# SpaceInvaders
+
+![start a game](img/start.png)
+
+> This is what it looks like when we play a game
+
+![playing a game](img/playing.png)
+
+> when you lose a game
+
+![first move in a game](img/gameOver.png)
+
+> We have a winner
+
+![player is winner](img/winning.png)
--- Level 2: Intermediate/spaceInvaders/index.js
+++ Level 2: Intermediate/spaceInvaders/index.js
@@ -9,19 +9,22 @@ let goingRight = true;
 let aliensRemoved = [];
 let results = 0;
 
+// We create a space in which we will play
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div');
     grid.appendChild(square)
 }
 
+// save all DIVs in Array 
 const squares = Array.from(document.querySelectorAll('.grid div'))
 
+// build alienInvaders
 const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ];
-
+// draw invaders
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         if (!aliensRemoved.includes(i)) {
@@ -32,6 +35,7 @@ function draw() {
 
 draw();
 
+// remove invader
 function removeDraw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         squares[alienInvaders[i]].classList.remove('invader');
@@ -39,8 +43,10 @@ function removeDraw() {
     }
 }
 
+// add shooter to game
 squares[currentShooterIndex].classList.add('shooter')
 
+// move shooter
 function moveShooter(e) {
     squares[currentShooterIndex].classList.remove('shooter')
     switch (e.key) {
@@ -50,55 +56,55 @@ function moveShooter(e) {
         case "ArrowRight":
             if (currentShooterIndex % width < width - 1) currentShooterIndex++
             break;
-
     }
     squares[currentShooterIndex].classList.add('shooter')
 }
-
+// waiting for any keydown events to move shooter
 document.addEventListener('keydown', moveShooter)
 
 function moveInvaders() {
+    // if it return true that mean the invaders in the left edge of the screen
     const leftEdge = alienInvaders[0] % width == 0;
+    // if it return true that mean the invaders in the right edge of the screen
     const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
     removeDraw();
+    // check if Invaders in the left edge of the screen
     if (rightEdge && goingRight) {
         for (let i = 0; i < alienInvaders.length; i++) {
+            // move all the invaders down
             alienInvaders[i] += width + 1;
+            // change the direction
             direction = -1;
+            // change the value of goingRight because we going to left now
             goingRight = false;
         }
+        // check if Invaders in the right edge of the screen
     } else if (leftEdge && !goingRight) {
         for (let i = 0; i < alienInvaders.length; i++) {
+            // move all the invaders down
             alienInvaders[i] += width - 1;
+            // change the direction
             direction = 1;
+            // change the value of goingRight because we going to left now
             goingRight = true;
         }
     }
+    // change the direction of the invaders
     for (let i = 0; i < alienInvaders.length; i++) {
         alienInvaders[i] += direction;
     }
 
-    /*for (let i = 0; i < alienInvaders.length; i++) {
-        if (rightEdge && goingRight) {
-            alienInvaders[i] += width + 1;
-            direction = -1;
-            goingRight = false;
-        } else if (leftEdge && !goingRight) {
-            alienInvaders[i] += width - 1;
-            direction = 1;
-            goingRight = true;
-        }
-        alienInvaders[i] += direction;
-    }*/
-
+    // draw the alien invaders
     draw()
 
     function GameOver() {
-        resultDisplay.innerHTML = 'GAME OVER!';
+        grid.innerHTML = 'GAME OVER!';
         clearInterval(invadersId);
         document.removeEventListener('keydown', moveShooter);
+        document.removeEventListener("keydown", shoot)
     }
 
+    // When invaders hit, the shooter. lose players
     if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
         GameOver()
     }
@@ -108,8 +114,9 @@ function moveInvaders() {
             GameOver()
         }
         if (aliensRemoved.length === alienInvaders.length) {
-            resultDisplay.innerHTML = 'YOU WIN!';
+            grid.innerHTML = 'YOU WIN!';
             clearInterval(invadersId);
+            document.removeEventListener('keydown', moveShooter);
         }
     }
 
@@ -136,7 +143,7 @@ function shoot(e) {
             const alienRemoval = alienInvaders.indexOf(currentLaserIndex);
             aliensRemoved.push(alienRemoval)
             results++
-            resultDisplay.innerHTML = results;
+            resultDisplay.innerHTML = 'Your score is : ' + results;
             console.log(aliensRemoved)
         }
     }
